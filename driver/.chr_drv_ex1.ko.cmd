82fef47e2377914eed2fbfb6b2be9de5f0c8a3be
saeed <saeedpourghasemy@gmail.com>
2021-06-28 15:46:36 +0430

    2021/06/28

--- driver/.chr_drv_ex1.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/embedded/test/device-driver/driver/chr_drv_ex1.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000  --build-id  -T ./scripts/module-common.lds -o /root/embedded/test/device-driver/driver/chr_drv_ex1.ko /root/embedded/test/device-driver/driver/chr_drv_ex1.o /root/embedded/test/device-driver/driver/chr_drv_ex1.mod.o;  true
--- driver/.chr_drv_ex1.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/embedded/test/device-driver/driver/chr_drv_ex1.mod := { echo  /root/embedded/test/device-driver/driver/chr_drv_ex1.o; echo; } > /root/embedded/test/device-driver/driver/chr_drv_ex1.mod
